7c6806ca1381d16a640cb5c29b43448b575a031e
pratapchowdari kolla <kollapratapchowdari@gmail.com>
2020-03-28 12:33:27 -0400

    xml based configuration changes for spring framework

--- src/main/java/com/example/jokesapp/JokesAppApplication.java
+++ src/main/java/com/example/jokesapp/JokesAppApplication.java
@@ -2,8 +2,10 @@ package com.example.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:check-config.xml")
 public class JokesAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(JokesAppApplication.class, args);
